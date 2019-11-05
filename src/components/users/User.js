7a9c9b3711645cfd1b6128e04f24528a784d8019
“drtoil” <axmed13@hotmail.com>
2019-11-05 02:27:17 +0000

    pulled data from user

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -5,7 +5,22 @@ class User extends Component {
     this.props.getUser(this.props.match.params.login);
   }
   render() {
-    return <div>User</div>;
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gist,
+      hireable
+    } = this.props.user;
+    const { loading } = this.props;
+    return <div>{name}</div>;
   }
 }
 
