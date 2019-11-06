f725ec11e98420e1b38f6c5b9b4a2d59be2969d3
“drtoil” <axmed13@hotmail.com>
2019-11-06 21:38:12 +0000

    working on user profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -34,6 +34,35 @@ class User extends Component {
         <Link to='/' className='btn btn-light'>
           Back to Search
         </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              visit Github Profile
+            </a>
+          </div>
+        </div>
       </Fragment>
     );
   }
