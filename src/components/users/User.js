24ec39f9adec559dc2ed7ff827f4e8da91178291
“drtoil” <axmed13@hotmail.com>
2019-11-06 16:18:30 +0000

    back to search button added

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
   componentDidMount() {
@@ -27,7 +28,14 @@ class User extends Component {
       hireable
     } = this.props.user;
     const { loading } = this.props;
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+      </Fragment>
+    );
   }
 }
 
