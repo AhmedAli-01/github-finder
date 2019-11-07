95ff2e069cab576f4441fc7beec618468a61464a
“drtoil” <axmed13@hotmail.com>
2019-11-07 15:33:57 +0000

    added username, company & website of the user if they exist

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -15,6 +15,7 @@ class User extends Component {
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -61,6 +62,29 @@ class User extends Component {
             <a href={html_url} className='btn btn-dark my-1'>
               visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
       </Fragment>
