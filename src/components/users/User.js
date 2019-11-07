642d198e12223b55a4c7dde632c2201ac0cd9234
“drtoil” <axmed13@hotmail.com>
2019-11-07 15:51:25 +0000

    added new div with follower, following, pub-rep, pub-gist

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -87,6 +87,12 @@ class User extends Component {
             </ul>
           </div>
         </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gist}</div>
+        </div>
       </Fragment>
     );
   }
